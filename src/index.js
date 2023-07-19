7037e610f356bffbb4ba6066667c7ad9d747b346
KGallo31 <Gallok731@gmail.com>
2023-07-19 19:27:34 -0400

    Added fetching number of entires from DOM

--- src/index.js
+++ src/index.js
@@ -1,6 +1,40 @@
 require("dotenv").config();
 const { Client, IntentsBitField } = require("discord.js");
 
+
+
+async function makeGetRequest(url, m) {
+  try {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      m.reply("That url stinks")
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const data = await response.text();
+
+    const entireEntryString = data.substring(data.indexOf("workshopBrowsePagingInfo") + 26, data.indexOf("workshopBrowsePagingInfo") + 50).trim()
+
+    const totalEntires = entireEntryString.substring(entireEntryString.indexOf("of") + 2,entireEntryString.indexOf("of") + 5).trim()
+
+    m.reply("The current total amount of entires for that user is: " + totalEntires)
+
+  } catch (error) {
+    console.error('Error:', error);
+    throw error;
+  }
+}
+
+async function GetTotalEntires(m) {
+  try {
+    const url = 'https://steamcommunity.com/id/Calwaelf/myworkshopfiles/?appid=311210';
+    await makeGetRequest(url,m);
+  } catch (error) {
+    m.reply("Oh shit I broke")
+  }
+}
+
 const client = new Client({
   intents: [
     IntentsBitField.Flags.Guilds,
@@ -10,14 +44,13 @@ const client = new Client({
   ],
 });
 
-const interval = 1000;
 
 client.on("messageCreate", (m) => {
   if (m.author.bot) {
     return;
-  }
-  m.reply("hello");
+  } 
+  // TODO: Allow the bot to read the message before it replies. It will look for a specific key word before it replies with the total amount of entires.  
+  GetTotalEntires(m)
 });
 
-client.login( process.env.TOKEN
-);
+client.login( process.env.TOKEN );
